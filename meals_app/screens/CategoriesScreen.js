09f6d4e5be8133d1f7c1b7468c5ef007c2fed577
yousikJeon <andrewskej@gmail.com>
2020-07-22 17:34:31 -0400

    grid design

--- meals_app/screens/CategoriesScreen.js
+++ meals_app/screens/CategoriesScreen.js
@@ -1,46 +1,33 @@
 import React from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
-import Colors from '../constants/Colors';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
-    const renderGridItem = (itemData) => {
+    const renderGridItem = itemData => {
         return (
-                <TouchableOpacity 
-                    style={styles.gridItem} 
-                    onPress={()=>{
-                    props.navigation.navigate({routeName:'CategoryMeals', params: {
-                        categoryId: itemData.item.id
-                    }})
-                }}>
-                    <View>
-                        <Text>{itemData.item.title}</Text>
-                    </View>
-                </TouchableOpacity>
-                );
+            <CategoryGridTile 
+                title={itemData.item.title} 
+                color={itemData.item.color}
+                onSelect={()=> {
+                    props.navigation.navigate({
+                        routeName:'CategoryMeals', 
+                        params: { categoryId: itemData.item.id }
+                        })
+                }
+            }/>
+            )
     }
 
     return (
         <FlatList 
+            keyExtractor={(item, index) => item.id} //unnecessary in newer version of RN!
             data={CATEGORIES} 
             renderItem={renderGridItem} 
             numColumns={2}
-        />
+        /> 
     )
 };
 
-
-const styles = StyleSheet.create({
-    screen:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
-    },
-    gridItem:{
-        flex:1,
-        margin:15,
-        height: 150
-    }
-})
 
 export default CategoriesScreen;
